beae9e261f7183b658fdba99374d1236a6452acc
Shovon.fuzion <shovon.fuzion@gmail.com>
2022-02-28 18:37:39 +0600

    customize banner added

--- components/ShopManagement/ShopBanner/ShopBanner.js
+++ components/ShopManagement/ShopBanner/ShopBanner.js
@@ -1,36 +1,48 @@
 import Image from "next/image";
-import banner from "../../../public/678.jpg";
+import banner from "../../../public/admin.png";
 import banner1 from "../../../public/remove-bg-678.jpg";
 import banner2 from "../../../public/bg-image.png";
 
 const ShopBanner = () => {
   return (
-    <div className="container mx-auto relative">
+    <div className="bg-gradient-to-tl from-gray-200 via-slate-200 to-gray-400 relative">
       <Image
-        src={banner2}
-        // className="opacity-80"
+        src={banner}
+        className="absolute mix-blend-multiply"
+        height={700}
         alt="Banner"
-        width={1250}
-        height={900}
+        layout="responsive"
       />
-      <div className="absolute top-0 md:top-5 bg-slate-300 p-4 rounded-lg bg-opacity-40">
-        <h3 className="text-center text-xl md:text-3xl lg:text-4xl xl:text-5xl text-gray-700 py-2">
-          Swadesh Shop Pannel
-        </h3>
-        <p className="md:text-lg text-blue-500">Your future is here</p>
-      </div>
-      <div className="absolute bottom-60 w-full">
-        <div className="w-1/2 float-right bg-slate-400 text-center lg:p-4 rounded-lg bg-opacity-50">
-          <h4 className="text-4xl font-bold">Search:</h4>
+      <div className="absolute top-10 w-full flex justify-start items-start px-20 py-24">
+        <div className="w-1/2">
+          <h4 className="text-4xl mr-4 font-semibold text-gray-800">
+            Search Your Shop:
+          </h4>
           <input
             type="text"
             placeholder="Shop Name/ID No./Mobile Number"
-            className="my-3 w-2/3 px-3 py-2 border border-green-400 rounded-lg"
+            className="mt-3 mb-32 w-full px-3 py-2 border border-green-400 rounded-lg focus:outline-none"
           />
           <br />
-          <button className="px-10 py-3 bg-yellow-400 rounded-lg font-bold">
-            ADD Store
-          </button>
+          <div className="">
+            <h3 className="font-semibold text-xl md:text-3xl lg:text-4xl xl:text-4xl text-gray-700 py-2">
+              Swadesh Bangladesh Shop Pannel
+            </h3>
+            {/* <p className="md:text-lg my-5">
+              The Administration Panel (or the admin panel for short) is the
+              primary tool for you to work with your online store. Here you can
+              manage products and orders, offer discounts, interact with your
+              customers, change the look of your store and do much more.
+            </p> */}
+          </div>
+          <div className="w-full mt-10">
+            <button className="mr-4 px-10 py-3 bg-yellow-300 rounded-lg font-bold hover:bg-yellow-400 hover:text-white">
+              ADD STORE
+            </button>
+            <button className="px-10 py-3 bg-green-300 rounded-lg font-bold hover:bg-green-400 hover:text-white">
+              ABOUT SWADESH
+            </button>
+          </div>
         </div>
       </div>
     </div>
